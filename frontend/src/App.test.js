e378dedfd8ec0f9f6fe23c3f6616aff501573486
Peter T <petertao9t@gmail.com>
2021-10-02 14:16:24 -0400

    fix the test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 test('renders learn react link', () => {
   console.log(`========= Frontend: I am running a test! =========`);
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I am running!/i);
   expect(linkElement).toBeInTheDocument();
 });
