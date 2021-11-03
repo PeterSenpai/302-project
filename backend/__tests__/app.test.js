1576b2274451c66c3e0a0128469e4e749cf7a82d
llolic <lazarlolic12@gmail.com>
2021-11-03 13:38:10 -0400

    Add backend test for feature 2

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -1,6 +1,23 @@
 import request from 'supertest';
 import app from '../app.js';
 
+const exampleResponse = {
+  resourceType: 'QuestionnaireResponse',
+  item: [
+    {
+      linkId: '1',
+      text: 'Are you a resident of, or do you travel frequently to, an area with active Zika transmission?',
+      answer: [ { valueBoolean: true } ]
+    },
+    {
+      linkId: '2',
+      text: 'How long has it been since you returned?',
+      answer: [ { valueString: '10 days' } ] 
+    }
+  ],
+  status: 'in-progress'
+}
+
 describe('GET /test', () => {
   it('should return welcome text', async () => {
     console.log(`========= Backend: I am running a test! =========`);
@@ -29,5 +46,13 @@ describe('GET /api/questionnaires', () => {
           expect(res.body[i].resourceType).toBe('Questionnaire');
         }
       });
+  });
+});
+
+describe('POST /api/response', () => {
+  it('should receive questionnaire response', async() => {
+    const res = await request(app).post('/api/response')
+      				    .send(exampleResponse)
+    expect(res.statusCode).toBe(200);
   });
 });
