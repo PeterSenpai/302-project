6b6f81881b6d5b9888c48ec6ef9c95cb7daef5eb
llolic <lazarlolic12@gmail.com>
2021-12-03 09:04:21 -0500

    Add frontend tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -7,58 +7,51 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('placeholder', () => {
+test('has question 1', () => {
   console.log(`========= Frontend: I am running a test! =========`);
   render(<App />);
   const Element = screen.getByText(/Name of child/i);
   expect(Element).toBeInTheDocument();
 });
 
-// test('has zika question 1', () => {
-//   console.log(`========= Frontend: I am running a test! =========`);
-//   render(<App />);
-//   const linkElement = screen.getByText(
-//     /Are you a resident of, or do you travel frequently to, an area with active Zika transmission?/i
-//   );
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('has question 2', () => {
+  render(<App />);
+  const Element = screen.getByText(/Sex/i);
+  expect(Element).toBeInTheDocument();
+});
 
-// test('has zika question 2', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(
-//     /Have you recently traveled to an area with active Zika transmission?/i
-//   );
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('has question 3', () => {
+  render(<App />);
+  const Element = screen.getByText(/Birth weight (kg)/i);
+  expect(Element).toBeInTheDocument();
+});
 
-// test('has zika question 3', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(
-//     /How long has it been since you returned?/i
-//   );
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('has question 4', () => {
+  render(<App />);
+  const Element = screen.getByText(/Birth length (cm)/i);
+  expect(Element).toBeInTheDocument();
+});
 
-// test('has zika question 4', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(
-//     /Have you recently had condomless sex with a partner that has travelled in an area with active Zika transmission?/i
-//   );
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('has question 5', () => {
+  render(<App />);
+  const Element = screen.getByText(/Vitamin K given/i);
+  expect(Element).toBeInTheDocument();
+});
 
-// test('has zika question 5', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(
-//     /How long has it been since your last condomless sexual encounter?/i
-//   );
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('has question 6', () => {
+  render(<App />);
+  const Element = screen.getByText(/Hep B given y \/ n/i);
+  expect(Element).toBeInTheDocument();
+});
 
-// test('has zika question 6', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(
-//     /Do you plan to travel to an area with active Zika transmission?/i
-//   );
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('has question 7', () => {
+  render(<App />);
+  const Element = screen.getByText(/Date given/i);
+  expect(Element).toBeInTheDocument();
+});
+
+test('has question 8', () => {
+  render(<App />);
+  const Element = screen.getByText(/Abnormalities noted at birth/i);
+  expect(Element).toBeInTheDocument();
+});
