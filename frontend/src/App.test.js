8c7094aa701f1a2878a7d16731ee65584e3eb02a
llolic <lazarlolic12@gmail.com>
2021-12-03 23:34:44 +0100

    Work on frontend tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,80 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { updateValueByLinkIdAndResponseType, extractItems } from './questionnaireParser';
-
-const questionnaire = {
-    "resourceType": "Questionnaire",
-    "id": "bb",
-    "text": {
-      "status": "generated",
-      "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\">\n      <pre>\n        <b>Birth details - To be completed by health professional</b>\n  Name of child: ____________________________________\n            Sex: __\n            \n  Neonatal Information\n    Birth Weight (kg): ___________\n    Birth Length (cm): ___________\n    Vitamin K given  : __\n             1st dose: ___________\n             2nd dose: ___________\n    Hep B given      : __\n      Date given     : ___________\n    Abnormalities noted at birth:\n      _______________________________________________\n      </pre>\n    </div>"
-    },
-    "url": "http://hl7.org/fhir/Questionnaire/bb",
-    "title": "NSW Government My Personal Health Record",
-    "status": "draft",
-    "subjectType": ["Patient"],
-    "date": "2013-02-19",
-    "publisher": "New South Wales Department of Health",
-    "jurisdiction": [
-      {
-        "coding": [
-          {
-            "system": "urn:iso:std:iso:3166",
-            "code": "AU"
-          }
-        ]
-      }
-    ],
-    "item": [
-      {
-        "linkId": "birthDetails",
-        "text": "Birth details - To be completed by health professional",
-        "type": "group",
-        "item": [
-          {
-            "linkId": "group",
-            "type": "group",
-            "item": [
-              {
-                "linkId": "nameOfChild",
-                "text": "Name of child",
-                "type": "string"
-              },
-              {
-                "linkId": "sex",
-                "text": "Sex",
-                "type": "choice",
-                "answerOption": [
-                  {
-                    "valueCoding": {
-                      "code": "F"
-                    }
-                  },
-                  {
-                    "valueCoding": {
-                      "code": "M"
-                    }
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            "linkId": "neonatalInformation",
-            "text": "Neonatal Information",
-            "type": "group",
-            "item": [
-              {
-                "linkId": "birthWeight",
-                "text": "Birth weight (kg)",
-                "type": "decimal"
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
+import mockData from './mockQuestionnires.json';
 
 const expected_questions = [
   {
@@ -106,6 +33,180 @@ const expected_questions = [
     "text": "Birth weight (kg)",
     "type": "decimal",
     "responseType": "valueDecimal"
+  },
+  {
+    "linkId": "birthLength",
+    "text": "Birth length (cm)",
+    "type": "decimal",
+    "responseType": "valueDecimal"
+  },
+  {
+    "linkId": "vitaminKgiven",
+    "text": "Vitamin K given",
+    "type": "choice",
+    "answerOption": [
+      {
+        "valueCoding": {
+          "code": "INJECTION"
+        }
+      },
+      {
+        "valueCoding": {
+          "code": "INTRAVENOUS"
+        }
+      },
+      {
+        "valueCoding": {
+          "code": "ORAL"
+        }
+      }
+    ],
+    "item": [
+      {
+        "linkId": "vitaminKgivenDoses",
+        "type": "group",
+        "enableWhen": [
+          {
+            "question": "vitaminKgiven",
+            "operator": "exists",
+            "answerBoolean": true
+          }
+        ],
+        "item": [
+          {
+            "linkId": "vitaminKDose1",
+            "text": "1st dose",
+            "type": "dateTime",
+            "enableWhen": [
+              {
+                "question": "vitaminKgiven",
+                "operator": "exists",
+                "answerBoolean": true
+              }
+            ],
+            "responseType": "valueDateTime"
+          },
+          {
+            "linkId": "vitaminKDose2",
+            "text": "2nd dose",
+            "type": "dateTime",
+            "enableWhen": [
+              {
+                "question": "vitaminKgiven",
+                "operator": "exists",
+                "answerBoolean": true
+              }
+            ],
+            "responseType": "valueDateTime"
+          },
+          {
+            "linkId": "vitaminKDose3ForTesting",
+            "text": "3rd dose (for testing)",
+            "type": "date",
+            "enableWhen": [
+              {
+                "question": "vitaminKgiven",
+                "operator": "exists",
+                "answerBoolean": true
+              }
+            ],
+            "responseType": "valueDate"
+          },
+          {
+            "linkId": "vitaminKDose4ForTesting",
+            "text": "4th dose (for testing)",
+            "type": "time",
+            "enableWhen": [
+              {
+                "question": "vitaminKgiven",
+                "operator": "exists",
+                "answerBoolean": true
+              }
+            ],
+            "responseType": "valueTime"
+          }
+        ]
+      }
+    ],
+    "responseType": "valueCoding"
+  },
+  {
+    "linkId": "vitaminKDose1",
+    "text": "1st dose",
+    "type": "dateTime",
+    "enableWhen": [
+      {
+        "question": "vitaminKgiven",
+        "operator": "exists",
+        "answerBoolean": true
+      }
+    ],
+    "responseType": "valueDateTime"
+  },
+  {
+    "linkId": "vitaminKDose2",
+    "text": "2nd dose",
+    "type": "dateTime",
+    "enableWhen": [
+      {
+        "question": "vitaminKgiven",
+        "operator": "exists",
+        "answerBoolean": true
+      }
+    ],
+    "responseType": "valueDateTime"
+  },
+  {
+    "linkId": "vitaminKDose3ForTesting",
+    "text": "3rd dose (for testing)",
+    "type": "date",
+    "enableWhen": [
+      {
+        "question": "vitaminKgiven",
+        "operator": "exists",
+        "answerBoolean": true
+      }
+    ],
+    "responseType": "valueDate"
+  },
+  {
+    "linkId": "vitaminKDose4ForTesting",
+    "text": "4th dose (for testing)",
+    "type": "time",
+    "enableWhen": [
+      {
+        "question": "vitaminKgiven",
+        "operator": "exists",
+        "answerBoolean": true
+      }
+    ],
+    "responseType": "valueTime"
+  },
+  {
+    "linkId": "hepBgiven",
+    "text": "Hep B given y / n",
+    "type": "boolean",
+    "item": [
+      {
+        "linkId": "hepBgivenDate",
+        "text": "Date given",
+        "type": "date",
+        "responseType": "valueDate"
+      }
+    ],
+    "responseType": "valueBoolean"
+  },
+  {
+    "linkId": "hepBgivenDate",
+    "text": "Date given",
+    "type": "date",
+    "responseType": "valueDate"
+  },
+  {
+    "linkId": "abnormalitiesAtBirth",
+    "text": "Abnormalities noted at birth",
+    "type": "string",
+    "responseType": "valueString"
   }
 ];
 
@@ -145,6 +246,96 @@ const expected_response = [
                 "valueDecimal": ""
               }
             ]
+          },
+          {
+            "linkId": "birthLength",
+            "text": "Birth length (cm)",
+            "answer": [
+              {
+                "valueDecimal": ""
+              }
+            ]
+          },
+          {
+            "linkId": "vitaminKgiven",
+            "text": "Vitamin K given",
+            "answer": [
+              {
+                "valueCoding": "",
+                "item": [
+                  {
+                    "item": [
+                      {
+                        "linkId": "vitaminKDose1",
+                        "text": "1st dose",
+                        "answer": [
+                          {
+                            "valueDateTime": ""
+                          }
+                        ]
+                      },
+                      {
+                        "linkId": "vitaminKDose2",
+                        "text": "2nd dose",
+                        "answer": [
+                          {
+                            "valueDateTime": ""
+                          }
+                        ]
+                      },
+                      {
+                        "linkId": "vitaminKDose3ForTesting",
+                        "text": "3rd dose (for testing)",
+                        "answer": [
+                          {
+                            "valueDate": ""
+                          }
+                        ]
+                      },
+                      {
+                        "linkId": "vitaminKDose4ForTesting",
+                        "text": "4th dose (for testing)",
+                        "answer": [
+                          {
+                            "valueTime": ""
+                          }
+                        ]
+                      }
+                    ],
+                    "linkId": "vitaminKgivenDoses"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "linkId": "hepBgiven",
+            "text": "Hep B given y / n",
+            "answer": [
+              {
+                "valueBoolean": "",
+                "item": [
+                  {
+                    "linkId": "hepBgivenDate",
+                    "text": "Date given",
+                    "answer": [
+                      {
+                        "valueDate": ""
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "linkId": "abnormalitiesAtBirth",
+            "text": "Abnormalities noted at birth",
+            "answer": [
+              {
+                "valueString": ""
+              }
+            ]
           }
         ],
         "linkId": "neonatalInformation"
@@ -152,15 +343,15 @@ const expected_response = [
     ],
     "linkId": "birthDetails"
   }
-]
+];
 
 test('render logic is correct', () => {
-  const questions = extractItems(questionnaire).question;
+  const questions = extractItems(mockData[1]).question;
   expect(questions).toEqual(expected_questions);
 });
 
 test('creating blank questionnaire response is correct', () => {
-  const resp = extractItems(questionnaire).item;
+  const resp = extractItems(mockData[1]).item;
   expect(resp).toEqual(expected_response);
 });
 
