4cb28be90742a0f18fc655c5eaaec09c2e82a270
llolic <lazarlolic12@gmail.com>
2021-12-04 14:36:59 -0500

    Add comments referring to validation criteria

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -345,34 +345,40 @@ const expected_response = [
   }
 ];
 
+// This test is validating the acceptance criteria of feature 2
 test('render logic is correct', () => {
   const questions = extractItems(mockData[1].item).questions;
   expect(questions).toEqual(expected_questions);
 });
 
+// This test is validating the acceptance criteria of feature 3
 test('creating blank questionnaire response is correct', () => {
   const resp = extractItems(mockData[1].item).item;
   expect(resp).toEqual(expected_response);
 });
 
+// This test is validating the acceptance criteria of feature 3
 test('updating string field in questionnaire response is correct', () => {
   let resp = extractItems(mockData[1].item).item;
   updateValueByLinkIdAndResponseType(resp, "abnormalitiesAtBirth", "valueString", "Test string");
   expect(resp[0].item[1].item[4].answer[0].valueString).toEqual("Test string");
 });
 
+// This test is validating the acceptance criteria of feature 3
 test('updating decimal field in questionnaire response is correct', () => {
   let resp = extractItems(mockData[1].item).item;
   updateValueByLinkIdAndResponseType(resp, "birthWeight", "valueDecimal", 5);
   expect(resp[0].item[1].item[0].answer[0].valueDecimal).toEqual(5);
 });
 
+// This test is validating the acceptance criteria of feature 3
 test('updating datetime field in questionnaire response is correct', () => {
   let resp = extractItems(mockData[1].item).item;
   updateValueByLinkIdAndResponseType(resp, "vitaminKDose1", "valueDateTime", "2021-12-18T00:08:28.142Z");
   expect(resp[0].item[1].item[2].answer[0].item[0].item[0].answer[0].valueDateTime).toEqual("2021-12-18T00:08:28.142Z");
 });
 
+// This test is validating the acceptance criteria of feature 3
 test('updating coding field in questionnaire response is correct', () => {
   let resp = extractItems(mockData[1].item).item;
   updateValueByLinkIdAndResponseType(resp, "sex", "valueCoding", "F");
@@ -380,6 +386,7 @@ test('updating coding field in questionnaire response is correct', () => {
 });
 
 //The following tests (has question 1 through 8) are for the specific questionnaire used. If the questionnaire is changed, the tests will need to be commented and new ones will need to be written for the specific questions
+// This test is validating the acceptance criteria of feature 2
 test('has question 1', () => {
   console.log("========= Frontend: I am running a test! =========");
   render(<App />);
@@ -387,42 +394,49 @@ test('has question 1', () => {
   expect(Element).toBeInTheDocument();
 });
 
+// This test is validating the acceptance criteria of feature 2
 test('has question 2', () => {
   render(<App />);
   const Element = screen.getByText(/Sex/i);
   expect(Element).toBeInTheDocument();
 });
 
+// This test is validating the acceptance criteria of feature 2
 test('has question 3', () => {
   render(<App />);
   const Element = screen.getByText(/Birth weight \(kg\)/i);
   expect(Element).toBeInTheDocument();
 });
 
+// This test is validating the acceptance criteria of feature 2
 test('has question 4', () => {
   render(<App />);
   const Element = screen.getByText(/Birth length \(cm\)/i);
   expect(Element).toBeInTheDocument();
 });
 
+// This test is validating the acceptance criteria of feature 2
 test('has question 5', () => {
   render(<App />);
   const Element = screen.getByText(/Vitamin K given/i);
   expect(Element).toBeInTheDocument();
 });
 
+// This test is validating the acceptance criteria of feature 2
 test('has question 6', () => {
   render(<App />);
   const Element = screen.getByText(/Hep B given y \/ n/i);
   expect(Element).toBeInTheDocument();
 });
 
+// This test is validating the acceptance criteria of feature 2
 test('has question 7', () => {
   render(<App />);
   const Element = screen.getByText(/Date given/i);
   expect(Element).toBeInTheDocument();
 });
 
+// This test is validating the acceptance criteria of feature 2
 test('has question 8', () => {
   render(<App />);
   const Element = screen.getByText(/Abnormalities noted at birth/i);
